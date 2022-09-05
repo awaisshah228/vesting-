9ba8686c88534f4efc238d668016a2d352221edf
awaisshah228 <m.awaisshah228@gmail.com>
2022-09-05 23:25:25 +0500

    donen

---
+++ contracts/MockTokenVesting.sol
@@ -0,0 +1,31 @@
+// contracts/TokenVesting.sol
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity 0.8.11;
+
+import "./TokenVesting.sol";
+
+/**
+ * @title MockTokenVesting
+ * WARNING: use only for testing and debugging purpose
+ */
+contract MockTokenVesting is TokenVesting{
+
+    uint256 mockTime = 0;
+
+    constructor(address token_) TokenVesting(token_){
+    }
+
+    function setCurrentTime(uint256 _time)
+        external{
+        mockTime = _time;
+    }
+
+    function getCurrentTime()
+        internal
+        virtual
+        override
+        view
+        returns(uint256){
+        return mockTime;
+    }
+}
